0018ae05e8ac60f5ba644c15d6eb1e5f40bdfcb6
tylarochele4-netizen <tylarochele4@gmail.com>
2026-04-21 00:13:46 +0300

    Add Footer component for site footer section

---
+++ src/components/Layout/footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer className="footer">
+            <p>&copy; {new Date().getFullYear()} CommunityHub. All rights reserved.</p>
+        </footer>
+    );
+}
+
+export default Footer;
